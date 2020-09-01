3d22b4d45aea4b8559657d82c4d1e3005ae8a1cd
vc-akashkadia <akash.kadia@viitor.cloud>
2020-09-01 10:26:59 +0530

    -Remove unused files

--- src/@utilities/index.js
+++
@@ -1,9 +0,0 @@
-export { default as AnimateLoading } from "./AnimateLoading";
-export { default as Toaster } from "./Toaster";
-export { default as Input } from "./Input";
-export { default as CheckBox } from "./CheckBox";
-
-export { default as Button } from "./ButtonUtils";
-export { default as DropDown } from "./DropDown/DropDown";
-export {default as Table} from "./Table"
-export {default as Modal} from "./YarnModal"
