16baa9883cac43acd44bd3d3a521662870e8bdd2
vc-akashkadia <akash.kadia@viitor.cloud>
2020-09-01 22:16:45 +0530

    -Changes the design of the facility page

--- src/pages/Facility.js
+++ src/pages/Facility.js
@@ -5,7 +5,7 @@ import { useHistory } from "react-router-dom";
 import brandLogo from "@assests/img/logo.png";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Alert from "@material-ui/lab/Alert";
@@ -15,16 +15,43 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import bottomImage from "@assests/img/pattern.svg";
-
+import InputBase from "@material-ui/core/InputBase";
+import MenuItem from "@material-ui/core/MenuItem";
 import {
   facilityListApiCall,
   selectedFacility,
 } from "../apicalls/FacilityApiCalls";
+import { Typography } from "@material-ui/core";
 
 let toasterOption = {
   option: "error",
   message: "Please select Facility",
 };
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    "label + &": {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: "#f6f6f6",
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    width: "100%",
+    // Use the system font instead of the default Roboto font.
+    fontFamily: ["Roboto"].join(","),
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}))(InputBase);
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -61,6 +88,12 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     left: 0,
   },
+  // label: {
+  //   fontSize: 14,
+  //   color: "#707070",
+  //   fontWeight: 500,
+  //   fontFamily: "Roboto",
+  // },
   button: {
     fontSize: 13,
     color: "#ffffff",
@@ -82,24 +115,24 @@ export default function Facility() {
   const [alert, setAlert] = useState(false);
   const [loading, setLoading] = useState(false);
   const [facility, setSelectFacility] = useState("");
-  const [errors, setErrors] = useState('');
+  const [errors, setErrors] = useState("");
   const authToken = useSelector(({ auth }) => auth.authToken);
   let facilityList = useSelector(({ base }) => base.facilityList);
   const dispatch = useDispatch();
   const history = useHistory();
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+
     setLoading(true);
-    if(facility !== ''){
-        dispatch(selectedFacility(facility));
-        history.push("/dashboard");
-    }else{
-        setErrors('Please select facility')
-        //setAlert(true)
-        setLoading(false);
+    if (facility !== "") {
+      dispatch(selectedFacility(facility));
+      history.push("/dashboard");
+    } else {
+      setErrors("Please select facility");
+      //setAlert(true)
+      setLoading(false);
     }
-   
+
     //dispatch(LoginApi(data,remember_me, handleCallback));
   };
   useEffect(() => {
@@ -118,7 +151,7 @@ export default function Facility() {
         option: "error",
         message: "Try After Some Time",
       };
-      setAlert(true)
+      setAlert(true);
       setLoading(false);
     }
   };
@@ -142,29 +175,39 @@ export default function Facility() {
             <Grid item xs={12} className={classes.listItemsChild}>
               {loading && <CircularProgress size={24} />}
               {!loading && (
-                <FormControl variant="filled" className={classes.formControl} error={errors !== ''}>
-                  <InputLabel htmlFor="filled-age-native-simple">
-                    Facility
-                  </InputLabel>
-                  <Select
-                    native
-                    value={facility}
-                    onChange={(e) => setSelectFacility(e.target.value)}
-                    inputProps={{
-                      name: "facility",
-                      id: "facility",
-                    }}
+                <>
+                  <Typography
+                    variant="h6"
+                    className={classes.formControl}
+                    style={{ textAlign: "center", paddingBottom: "10px" }}
                   >
-                    <option aria-label="None" value="" />
-                    <option value={10}>Ten</option>
-                    <option value={20}>Twenty</option>
-                    <option value={30}>Thirty</option>
-                    {facilityList.map((item) => (
-                      <option value={item.value}>{item.lable}</option>
-                    ))}
-                  </Select>
+                    Facility
+                  </Typography>
+                  <FormControl
+                    variant="filled"
+                    className={classes.formControl}
+                    error={errors !== ""}
+                  >
+                    <Select
+                      labelId="demo-customized-select-label"
+                      id="demo-customized-select"
+                      value={facility}
+                      onChange={(e) => setSelectFacility(e.target.value)}
+                      input={<BootstrapInput />}
+                      placeholder="Select Yard Crane"
+                      style={{ width: "100%" }}
+                    >
+                      {/* <MenuItem aria-label="None" value="" /> */}
+                      <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem>
+                      {facilityList.map((item) => (
+                        <MenuItem value={item.value}>{item.lable}</MenuItem>
+                      ))}
+                    </Select>
                     <FormHelperText>{errors}</FormHelperText>
-                </FormControl>
+                  </FormControl>
+                </>
               )}
             </Grid>
 
@@ -176,7 +219,7 @@ export default function Facility() {
                 className={classes.button}
                 disabled={loading}
               >
-               Ok {loading && <CircularProgress size={24} />}
+                Ok {loading && <CircularProgress size={24} />}
               </Button>
             </Grid>
           </form>
